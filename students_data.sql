dcfdd7a3535090f3f9f29736a0235d47365a9af2
UdayPrajapati17 <udayprajapati1718@gmail.com>
2025-12-09 15:41:25 +0530

    Add files via upload

--- students_data.sql
+++ students_data.sql
@@ -6,14 +6,32 @@ CREATE TABLE students (
     enrollment_id INT PRIMARY KEY,
     s_name VARCHAR(20),
     courses VARCHAR(25),
-    m_number VARCHAR(15)
+    age TINYINT
 );
 
 INSERT INTO students VALUES
-(1, 'Abhimanyu', 'Computer Science', '+91 9123654321'),
-(2, 'Parth', 'Information Technolodgy', '+91 1123456780'),
-(3, 'Ram', 'Maths', '+91 3412779955'),
-(4, 'Bhishm', 'LLB', '+91 1234567890'),
-(5, 'Karn', 'Buisness', '+91 9080706050');
+(20250001, 'Abhimanyu', 'Computer Science', 19),
+(20250002, 'Parth', 'Information Technolodgy',19 ),
+(20250003, 'Ram', 'Maths', 22),
+(20250004, 'Bhishm', 'LLB', 20),
+(20250005, 'Karn', 'Buisness', 21);
+
+INSERT INTO students VALUES
+(20250006, 'Laxman', 'Data Science', 19),
+(20250007, 'Bhim', 'Information Technolodgy',20 ),
+(20250008, 'Maruti', 'Maths', 21),
+(20250009, 'Madhusudan', 'Cyber Security', 21),
+(20250010, 'Krishn', 'B.tech(cs)', 19);
 
 SELECT * FROM students;
+
+SELECT * FROM students WHERE age>20;
+
+SELECT * FROM students WHERE courses="Data Science";
+
+SELECT * FROM students WHERE s_name LIKE "M%";
+
+SELECT * FROM students WHERE age BETWEEN 18 AND 25;
+
+SELECT * FROM students WHERE s_name="Abhimanyu";
+
